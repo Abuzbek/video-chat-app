08f1f31bde3dcfa33a90ade82c364fa445b75821
AbuzCoder <68583672+Abuzbek@users.noreply.github.com>
2023-06-27 09:24:48 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
-    setTimeout(()=>{
+    // setTimeout(()=>{
       socket.to(roomId).broadcast.emit("user-connected", userId);
-    }, 1000)
+    // }, 1000)
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message, userName);
     });
